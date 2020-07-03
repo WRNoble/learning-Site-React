df9744d350ca3ef629224985ea67408493ec73fe
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-03 12:46:54 -0400

    teacher and courses route

--- course-directory/src/components/App.js
+++ course-directory/src/components/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'; 
 import Home from './Home';
 import About from './About';
+import Header from './Header';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
+      <Header />
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
+      <Route exact path="/teachers" component={Teachers} />
+      <Route exact path="/courses" component={Courses} />
     </div>
   </BrowserRouter>
 );
