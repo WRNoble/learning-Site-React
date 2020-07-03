579f032eb0db79de3f4ab6306604b71da260d427
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-03 15:04:28 -0400

    finished

--- course-directory/src/components/App.js
+++ course-directory/src/components/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'; 
+import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 import Home from './Home';
 import About from './About';
 import Header from './Header';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
+import Featured from './Featured';
+
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header /> 
       {/* header is included on all routes so it is left outside the routes */}
-      <Route exact path="/" component={Home} />
-      <Route path="/about" render={ () => <About title="About" /> } />
-      <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" render={ () => <About title="About" /> } />
+        <Route exact path="/teachers" component={Teachers} />
+        <Route path="/teachers/:topic/:name" component={Featured} />
+        <Route path="/courses" component={Courses} />
+        <Route component={NotFound}/>
+      </Switch>
     </div>
   </BrowserRouter>
 );
